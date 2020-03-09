5b8d50783f74244c572ca91afaddddd1f03a8d5e
b606 <besoa606@gmail.com>
2020-03-09 01:11:53 +0100

    merge squash new-master 2020/03/09

--- Notes/scripts/normalize-eol.sh
+++ Notes/scripts/normalize-eol.sh
@@ -3,4 +3,4 @@ set -ex
 
 cd $(dirname $(readlink -f $0))/../..
 
-find Backstories DefInjected Keyed Notes Strings -name "*.xml" -o -name "*.txt" | xargs dos2unix --keep-bom
+find Core Royalty Notes -name "*.xml" -o -name "*.txt" | xargs dos2unix --keep-bom
